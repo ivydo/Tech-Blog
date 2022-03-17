4c2a330dec2d260f9ccf7d29fb2e576ac0a78769
Ivy Do <ivy.do2011@gmail.com>
2022-03-17 15:45:26 -0400

    seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,45 +2,45 @@ const { User } = require('../models');
 
 const userData = [
     {
-        username: "martin_bour",
-        twitter: "martinb",
-        github: "martinb",
-        email: "martin_b@gmail.com",
+        username: "kelly_k",
+        twitter: "kelly_k",
+        github: "kelly_k",
+        email: "kelly_k@gmail.com",
         password: "p@ssword1"
     },
     {
-        username: "matt_b",
-        twitter: "mathewb",
-        github: "mathewb",
-        email: "mathew_b@gmail.com",
+        username: "rachel_r",
+        twitter: "rachel_r",
+        github: "rachel_r",
+        email: "rachel_r@gmail.com",
         password: "p@ssword2"
     },
     {
-        username: "shaun_c",
-        twitter: "shaun",
-        github: "shaun",
-        email: "shaun_c@gmail.com",
+        username: "jjay",
+        twitter: "jjay",
+        github: "jjay",
+        email: "jjay@gmail.com",
         password: "p@ssword3"
     },
     {
-        username: "lee_n",
-        twitter: "leenorris",
-        github: "leenorris",
-        email: "lee_n@gmail.com",
+        username: "lee_kim",
+        twitter: "lee_kim",
+        github: "lee_kim",
+        email: "lee_kim@gmail.com",
         password: "p@ssword4"
     },
     {
-        username: "priya_r",
-        twitter: "priyaravi23",
-        github: "priyaravi23",
-        email: "priya_r@gmail.com",
+        username: "j_smith",
+        twitter: "jsmith",
+        github: "jsmith",
+        email: "jsmith@gmail.com",
         password: "p@ssword5"
     },
     {
-        username: "pooja",
-        twitter: "pooja_w",
-        github: "pooja",
-        email: "pooja@gmail.com",
+        username: "brit_s",
+        twitter: "brit_s",
+        github: "brit_s",
+        email: "brit_s@gmail.com",
         password: "p@ssword6"
     }
 ]
